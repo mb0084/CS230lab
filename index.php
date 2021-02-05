2ea1e076f5439f3aa487629e21945938e0dda839
mb0084 <mb0084@mix.wvu.edu>
2021-02-05 18:31:08 -0500

    Lab 2: about page

---
+++ index.php
@@ -0,0 +1,50 @@
+<body>
+    <link rel="stylesheet" href="css/about.css">
+
+    <div class="about">
+        <h1>About Us</h1>
+        <p>uihfuifhvuifdhvuifdhvudihvhf</p>
+    </div>
+    <div class="row">
+        <div class="column">
+            <div class="card">
+                <img src="images/MasterChief.jpeg">
+                <div class="container">
+                    <h2>CEO</h2>
+                    <p>Master Chief</p>
+                    <p>Email: John117@rings.com</p>
+                </div>
+            </div>
+        </div>
+        <div class="column">
+            <div class="card">
+                <img src="images/Arbiter.jpeg">
+                <div class="container">
+                    <h2>Accountant</h2>
+                    <p>The Arbiter</p>
+                    <p>Email: Disgraced@hichair.com</p>
+                </div>
+            </div>
+        </div>
+        <div class="column">
+            <div class="card">
+                <img src="images/Zavala.jpeg">
+                <div class="container">
+                    <h2>Animal Trainer</h2>
+                    <p>Zavala</p>
+                    <p>Email: zman@thelastcity.com</p>
+                </div>
+            </div>
+        </div>
+        <div class="column">
+            <div class="card">
+                <img src="images/Cayde6.jpeg">
+                <div class="container">
+                    <h2>Sales Associate</h2>
+                    <p>Cayde 6</p>
+                    <p>Email: ace@thelastcity.com</p>
+                </div>
+            </div>
+        </div>
+    </div>
+</body>
